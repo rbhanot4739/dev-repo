629baa9f2a898ebf6bc9d9025bba09b58cbda6f4
rbhanot <rbhanot4739@gmail.com>
2019-07-16 20:15:28 +0530

    Set up login n logout views

--- python3/django-projects/BlogProject/users/users_urls.py
+++ python3/django-projects/BlogProject/users/users_urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
-from .views import register, login
+from django.contrib.auth.views import login, logout
+
+from .views import register
 
 urlpatterns = [
     url(r'register/$', register, name='register-user'),
-    url(r'login/$', login, name='login-user')
+    url(r'login/$', login, {'template_name': "users/login.html"}, name='login'),
+    url(r'logout/', logout, {'next_page': 'login'}, name='logout')
 ]

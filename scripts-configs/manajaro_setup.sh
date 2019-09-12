af8ab40da3fc78e42c29adbf8b16906d49170fcc
Rohit Bhanot <rbhanot4739@gmail.com>
2019-09-12 23:43:24 +0530

    changed dir anmes

--- scripts-configs/manajaro_setup.sh
+++ scripts-configs/manajaro_setup.sh
@@ -43,7 +43,7 @@ then
 
 	echo -e "\n${YELLOW}..................... Installing packages .....................${NC}\n"
 
-	INSTALL_PACKAGES=" base-devel mod_wsgi apache python2-pip python-pip neovim unzip vuze nodejs npm vagrant"
+	INSTALL_PACKAGES=" base-devel mod_wsgi apache python2-pip python-pip neovim unzip vuze nodejs npm vagrant code"
 
 	for pkg in $INSTALL_PACKAGES;
 	do

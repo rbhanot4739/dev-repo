bc438f966d567ac375943b4a4bfc50d520761115
RohitB <rbhanot4739@gmail.com>
2019-11-07 18:17:23 +0530

    cleaned the repository

--- python3/miscellaneous/cml line args.py
+++
@@ -1,5 +0,0 @@
-import sys
-
-print("Name of the script is :  ", str(str(sys.argv[0]).split(":")[-1]).split("/")[-1])
-
-print("Other arguements of the script are: \n", "\n".join(sys.argv[:]), sep="")

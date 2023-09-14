bf932db60ee5a234ea3a0b4e58e7e294565bda5c
RohitB <rbhanot4739@gmail.com>
2023-09-14 17:09:31 +0530

    Update singletons.py

--- python3/oop/advanced_topics/metaclasses/singletons.py
+++ python3/oop/advanced_topics/metaclasses/singletons.py
@@ -2,20 +2,35 @@ class SingletonClassException(Exception):
     pass
 
 
+# class Singleton(type):
+
+#     def __new__(mcs, name, bases, clsdict):
+#         clsdict['_objcount'] = 0
+#         return super().__new__(mcs, name, bases, clsdict)
+
+#     def __call__(cls, *args, **kwargs):
+#         if cls._objcount >= 1:
+#             raise SingletonClassException(
+#                 "Singleton Class - Only one object can be created")
+#         else:
+#             cls._objcount += 1
+#             return super().__call__()
+
+
+# A more simpler implementation
 class Singleton(type):
 
     def __new__(mcs, name, bases, clsdict):
-        clsdict['_objcount'] = 0
+        clsdict['_obj'] = None
         return super().__new__(mcs, name, bases, clsdict)
 
     def __call__(cls, *args, **kwargs):
-        if cls._objcount >= 1:
-            raise SingletonClassException(
-                "Singleton Class - Only one object can be created")
-        else:
-            cls._objcount += 1
-            return super().__call__()
-
+        print("__call__")
+        if cls._obj:
+            return cls._obj
+        obj = super().__call__()
+        cls._obj = obj
+        return obj
 
 class Spam(object):
     pass
